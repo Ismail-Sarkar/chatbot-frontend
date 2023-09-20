e4d5893b4ec6ceb3e7be62100ee545cc4233cea9
Andres Mora <andres@devsenpai.com>
2023-09-20 16:10:36 -0600

    Update social branding

--- src/config/configBranding.js
+++ src/config/configBranding.js
@@ -1,8 +1,8 @@
-import logoImageDesktop from '../assets/biketribe-logo-desktop.png';
-import logoImageMobile from '../assets/biketribe-logo-mobile.png';
-import brandImage from '../assets/biketribe-brandImage-1500.jpg';
-import facebookImage from '../assets/biketribe-facebook-sharing-1200x630.jpg';
-import twitterImage from '../assets/biketribe-twitter-sharing-600x314.jpg';
+import logoImageDesktop from '../assets/adventurely-logo-desktop.png';
+import logoImageMobile from '../assets/adventurely-logo-mobile.png';
+import brandImage from '../assets/adventurely-brandImage-1500.jpg';
+import facebookImage from '../assets/adventurely-facebook-sharing-1200x630.jpg';
+import twitterImage from '../assets/adventurely-twitter-sharing-600x314.jpg';
 
 /////////////////////////////////////////////////////////
 // This file contains configs that affect branding     //
