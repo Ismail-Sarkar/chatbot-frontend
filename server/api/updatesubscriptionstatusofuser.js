ab3e52dc1690fcd0059836321f1c3c5154579598
sahaTanmoy <tanmoy.saha@bitcanny.com>
2023-11-01 17:48:57 +0530

    api added for webhook testing

---
+++ server/api/updatesubscriptionstatusofuser.js
@@ -0,0 +1,10 @@
+module.exports.updatesubscriptionstatusofuser = async (req, res) => {
+  try {
+    console.log('WebHook status: Hitted');
+    res.status(200).send({ message: 'WebHook status: Hitted' });
+  } catch (err) {
+    console.log('Error hitting webhook', err);
+    //   res.status(401).send({ message: 'Profile not updated', error: err.data.errors[0].source });
+    res.status(401).send({ message: 'Error hitting webhook', error: err });
+  }
+};
