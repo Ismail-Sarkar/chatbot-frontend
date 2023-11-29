cc7f804d3317ad24475cce8c78e48621a63225e7
Ismail-Sarkar <ismail.sarkar@bitcanny.com>
2023-11-29 19:15:51 +0530

    preview test commit

--- src/containers/ProfilePage/ProfilePage.duck.js
+++ src/containers/ProfilePage/ProfilePage.duck.js
@@ -274,7 +274,7 @@ export const ProfilePageByUserName = (params, search, config) => async (
 
 const delay = () => {
   return new Promise((resolve, reject) => {
-    setTimeout(resolve, 300);
+    setTimeout(resolve, 1000);
   });
 };
 
