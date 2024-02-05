4be13b2e8426a7924578b67815daae13106df6d6
sahaTanmoy <tanmoy.saha@bitcanny.com>
2024-02-05 13:51:22 +0530

    renewal api modification

--- server/api/updatesubscriptionstatusofuser.js
+++ server/api/updatesubscriptionstatusofuser.js
@@ -122,103 +122,107 @@ exports.renewSubscriptionofUser = async (req, res) => {
     if (req.body.type !== 'customer.subscription.updated')
       return res.status(200).send('not a subscription renewal');
 
-    const { id: subscriptionId } = req.body.data.object;
+    try {
+      const { id: subscriptionId } = req.body.data.object;
+      const userDetails = await integrationSdk.users.query({ prot_subscriptionId: subscriptionId });
 
-    const userDetails = await integrationSdk.users.query({ prot_subscriptionId: subscriptionId });
+      const userData = userDetails.data.data[0];
 
-    const userData = userDetails.data.data[0];
+      const { data } = req.body;
 
-    const { data } = req.body;
+      console.log('WebHook status: Hitted', req.body);
+      if (data) {
+        if (userData?.id?.uuid && data.object.object === 'subscription') {
+          try {
+            const subscription = await stripe.subscriptions.retrieve(data.object.subscription);
+            // console.log('Subscription Details:', subscription);
+            // console.log('Subscription Items:', subscription.items.data);
+            if (subscription) {
+              // Extract the required information from the subscription object
+              const status = subscription.status;
+              const startDate = new Date(subscription.current_period_start * 1000).toISOString(); // Convert to a JavaScript date
+              const endDate = new Date(subscription.current_period_end * 1000).toISOString(); // Convert to a JavaScript date
 
-    console.log('WebHook status: Hitted', req.body);
-    if (data) {
-      if (userData?.id?.uuid && data.object.mode === 'subscription' && data.object.subscription) {
-        try {
-          const subscription = await stripe.subscriptions.retrieve(data.object.subscription);
-          // console.log('Subscription Details:', subscription);
-          // console.log('Subscription Items:', subscription.items.data);
-          if (subscription) {
-            // Extract the required information from the subscription object
-            const status = subscription.status;
-            const startDate = new Date(subscription.current_period_start * 1000).toISOString(); // Convert to a JavaScript date
-            const endDate = new Date(subscription.current_period_end * 1000).toISOString(); // Convert to a JavaScript date
+              // Retrieve the subscription type information with amount
 
-            // Retrieve the subscription type information with amount
+              const subscriptionType = subscription.items.data.reduce((accumulator, item) => {
+                const type = item.price.type;
+                const unitAmount = item.price.unit_amount;
 
-            const subscriptionType = subscription.items.data.reduce((accumulator, item) => {
-              const type = item.price.type;
-              const unitAmount = item.price.unit_amount;
+                // If the type doesn't exist in the accumulator, initialize it to 0
+                if (!accumulator[type]) {
+                  accumulator[type] = 0;
+                }
 
-              // If the type doesn't exist in the accumulator, initialize it to 0
-              if (!accumulator[type]) {
-                accumulator[type] = 0;
-              }
+                // Add the unitAmount to the total for the current type
+                accumulator[type] += unitAmount;
 
-              // Add the unitAmount to the total for the current type
-              accumulator[type] += unitAmount;
+                return accumulator;
+              }, {});
 
-              return accumulator;
-            }, {});
-
-            const dataToUpdate = {
-              id: userData?.id?.uuid,
-              publicData: {
-                subScriptionOn: {
-                  subscriptionStart: startDate,
-                  subscriptionEnd: endDate,
+              const dataToUpdate = {
+                id: userData?.id?.uuid,
+                publicData: {
+                  subScriptionOn: {
+                    subscriptionStart: startDate,
+                    subscriptionEnd: endDate,
+                  },
                 },
-              },
-              protectedData: {
-                subscriptionDetails: {
-                  subscriptionId: data.object.subscription,
-                  subscriptionStatus: status,
-                  subscriptionStart: startDate,
-                  subscriptionEnd: endDate,
-                  subscriptionType: subscriptionType,
-                  subscriptionEmail: data.object.customer_details.email,
-                  subscriberName: data.object.customer_details.name,
-                  discountedPercent: subscription.discount?.coupon?.percent_off
-                    ? subscription.discount?.coupon?.percent_off
-                    : 0,
-                  discountAmount: subscription.discount?.coupon?.percent_off
-                    ? (subscriptionType.recurring * subscription.discount.coupon?.percent_off) / 100
-                    : 0,
-                  discountedAmount: subscription.discount?.coupon?.percent_off
-                    ? subscriptionType.recurring -
-                      (subscriptionType.recurring * subscription.discount?.coupon?.percent_off) /
+                protectedData: {
+                  subscriptionDetails: {
+                    subscriptionId: data.object.subscription,
+                    subscriptionStatus: status,
+                    subscriptionStart: startDate,
+                    subscriptionEnd: endDate,
+                    subscriptionType: subscriptionType,
+                    subscriptionEmail: data.object.customer_details.email,
+                    subscriberName: data.object.customer_details.name,
+                    discountedPercent: subscription.discount?.coupon?.percent_off
+                      ? subscription.discount?.coupon?.percent_off
+                      : 0,
+                    discountAmount: subscription.discount?.coupon?.percent_off
+                      ? (subscriptionType.recurring * subscription.discount.coupon?.percent_off) /
                         100
-                    : subscriptionType.recurring,
-                  totalAmount: subscriptionType.recurring,
+                      : 0,
+                    discountedAmount: subscription.discount?.coupon?.percent_off
+                      ? subscriptionType.recurring -
+                        (subscriptionType.recurring * subscription.discount?.coupon?.percent_off) /
+                          100
+                      : subscriptionType.recurring,
+                    totalAmount: subscriptionType.recurring,
+                  },
                 },
-              },
-            };
+              };
 
-            console.log('Subscription Details:', dataToUpdate.privateData.subscriptionDetails);
+              console.log('Subscription Details:', dataToUpdate.privateData.subscriptionDetails);
 
-            try {
-              const updatedProfile = await integrationSdk.users.updateProfile(dataToUpdate, {
-                expand: true,
-              });
-              //   res.status(200).send({ data: resp.data.data });
-              res.status(200).send({
-                message: 'Profile updated successfully',
-                subscriptionDetails: dataToUpdate,
-                updatedProfileDetails: updatedProfile,
-              });
-            } catch (err) {
-              console.log(69, err.data.errors[0].source);
-              res
-                .status(401)
-                .send({ message: 'Profile not updated', error: err.data.errors[0].source });
+              try {
+                const updatedProfile = await integrationSdk.users.updateProfile(dataToUpdate, {
+                  expand: true,
+                });
+                //   res.status(200).send({ data: resp.data.data });
+                res.status(200).send({
+                  message: 'Profile updated successfully',
+                  subscriptionDetails: dataToUpdate,
+                  updatedProfileDetails: updatedProfile,
+                });
+              } catch (err) {
+                console.log(69, err.data.errors[0].source);
+                res
+                  .status(401)
+                  .send({ message: 'Profile not updated', error: err.data.errors[0].source });
+              }
+            } else {
+              res.status(401).send({ message: 'Subscription data not found' });
             }
-          } else {
-            res.status(401).send({ message: 'Subscription data not found' });
+          } catch (err) {
+            console.log('Subscription data fetch error.....', err);
+            res.status(401).send({ message: 'Subscription data fetch error', error: err });
           }
-        } catch (err) {
-          console.log('Subscription data fetch error.....', err);
-          res.status(401).send({ message: 'Subscription data fetch error', error: err });
         }
       }
+    } catch (userFoundError) {
+      res.status(401).send({ message: 'User data not found', error: err.data.errors[0].source });
     }
   } catch (err) {
     console.log('Error hitting webhook', err);
